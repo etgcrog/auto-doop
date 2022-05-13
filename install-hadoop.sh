f19e75785ea7be2f143828463ec9436b17362ca3
Eduardo Teles Guimaraes <etgcrog@gmail.com>
2022-05-13 12:01:45 -0300

    ate as variaveis java3

--- install-hadoop.sh
+++ install-hadoop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add - echo "deb $([ \"$(uname -m)\" = \"x86_64\" ] && echo -n '[arch=amd64]') https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/ buster main" | sudo tee /etc/apt/sources.list.d/adoptopenjdk.list
-apt update
-apt install adoptopenjdk-8-hotspot -y
+wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
+sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
+sudo apt-get update && sudo apt-get install adoptopenjdk-8-hotspot
