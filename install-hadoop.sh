862ffca9fe42208bbaa2ad0267f327c927aa98aa
Eduardo Teles Guimaraes <etgcrog@gmail.com>
2022-05-13 11:50:34 -0300

    ate as variaveis java1

--- install-hadoop.sh
+++ install-hadoop.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd /opt
 apt-get install openjdk-8-jdk-headless -qq > /dev/nul -y
-mv jdk1.8.0_171 jdk
 echo export JAVA_HOME=/opt/jdk > ~/.bashrc
 echo export JRE_HOME=/opt/jdk/jre > ~/.bashrc
 echo export PATH=$PATH:$JAVA_HOME/bin:$JRE_HOME/bin > ~/.bashrc
