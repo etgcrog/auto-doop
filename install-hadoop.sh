1f1ae2d0cf4a3cf310f5a3b4a4dc1b980c71fee4
Eduardo Teles Guimaraes <etgcrog@gmail.com>
2022-05-16 21:44:14 -0300

    testando o novo token

--- install-hadoop.sh
+++ install-hadoop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#testando git
 wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
 sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
 sudo apt-get update && sudo apt-get install adoptopenjdk-8-hotspot
